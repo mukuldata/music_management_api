14be97fd82f5c56d185bbd66517157085bf3182f
Mukul Data <54991370+mukuldata@users.noreply.github.com>
2024-12-25 22:19:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./db/connection'); 
 const {responseHandler} =require("./utils/public-methods.utils")
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 8000;
 connectDB();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json()); 
 
 app.use(authRoutes);
